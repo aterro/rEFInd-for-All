8ff06f3d1f6d6724949bd68f5a84191351d0604f
Abdallah Terro <aterro@yahoo.com>
2026-05-14 08:54:17 +0300

    fixed for building with edk2 on linux

--- refind/Math.h
+++ refind/Math.h
@@ -1,7 +1,9 @@
 #ifndef _MATH_H_
 #define _MATH_H_
 
+#ifndef __BASE_LIB__
 UINT64 DivU64x64Remainder(UINT64 Dividend, UINT64 Divisor, UINT64 *Remainder);
 INT64 DivS64x64Remainder(INT64 Dividend, INT64 Divisor, INT64 *Remainder);
+#endif
 
 #endif
